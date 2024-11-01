e461d4bea726187579cac8af6de5ed06be43429f
Adrian Arce <arce.diaz.adrian@gmail.com>
2024-11-01 19:31:06 -0400

    creating posts, like and unlike with correct authorization

--- app/Http/Controllers/Posts/PostsController.php
+++ app/Http/Controllers/Posts/PostsController.php
@@ -3,12 +3,43 @@
 namespace App\Http\Controllers\Posts;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::latest()->with(['user', 'likes'])->paginate(10);
+
+        return view('posts.index', [
+            'posts' => $posts,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required',
+        ]);
+
+        $request->user()->posts()->create([
+            'body' => $request->body,
+        ]);
+
+        return back();
+    }
+
+    public function destroy(Post $post)
+    {
+        if (!Gate::allows('delete', $post)) {
+            abort(403);
+        }
+        
+        $post->delete();
+
+        return back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Posts\PostLikeController;
 use App\Http\Controllers\Posts\PostsController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,8 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/posts', [PostsController::class, 'index'])->name('posts');
+Route::post('/posts', [PostsController::class, 'store']);
+Route::delete('/posts/{post}', [PostsController::class, 'destroy'])->name('posts.destroy');
+
+Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
+Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy'])->name('posts.likes');
